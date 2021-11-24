836388e330b95f13e774e5c9cfa77359bb60c396
Zinou <zineddine.kermouz@redfabriq.com>
2021-11-24 13:40:55 +0100

    show correct and wrong answers

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ let selectedCategory = 'any';
 let selectedDifficulty = 'any';
 let questionCount = 0;
 let questionNumber = 1;
+let userScore = 0;
 
 function categoryChanged() {
     selectedCategory = category_select.value;
@@ -60,7 +61,7 @@ async function startQuiz() {
 
 function showQuetions(index) {
     let option_tag = '';
-    
+
     const currentQuestion = questions[index];
 
     let question_tag = `<span>${currentQuestion.numb} .${currentQuestion.question}</span>`;
@@ -69,17 +70,54 @@ function showQuetions(index) {
     })
 
     question_title.innerHTML = `<p>Question ${currentQuestion.numb}</p>`;
-    question_text.innerHTML = question_tag; 
+    question_text.innerHTML = question_tag;
     option_list.innerHTML = option_tag;
+
+    const optionNodeList = option_list.querySelectorAll(".option");
+
+    // set onclick attribute to all available options
+    optionNodeList.forEach(element => {
+        element.setAttribute("onclick", "optionSelected(this)");
+    });
+}
+
+function optionSelected(answer) {
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionCount].answer;
+    const allOptions = option_list.children.length;
+
+    if (userAnswer == correctAnswer) {
+        console.log("Correct Answer");
+        userScore += 1;
+        answer.classList.add("correct");
+    } else {
+        console.log("Wrong Answer");
+        answer.classList.add("incorrect");
+
+        // check for the correct answer and add green color ti it
+        for (i = 0; i < allOptions; i++) {
+            if (option_list.children[i].textContent == correctAnswer) {
+                console.log("Auto selected correct answer.");
+                option_list.children[i].setAttribute("class", "option correct");
+            }
+        }
+    }
+
+    //once user select an option then disabled all options
+    for (i = 0; i < allOptions; i++) {
+        option_list.children[i].classList.add("disabled");
+    }
+    //show the next button if user selected any option
+    next_btn.classList.add("show");
 }
 
 function nextQuestion() {
     //if question count is less than total question length
-    if (questionCount < questions.length - 1) { 
+    if (questionCount < questions.length - 1) {
         // show next question 
-        questionCount++; 
-        questionNumber++; 
-        showQuetions(questionCount); 
+        questionCount++;
+        questionNumber++;
+        showQuetions(questionCount);
         questionCounter(questionNumber);
     } else {
         // show results
