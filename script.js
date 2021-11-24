8188bdee83cbd4e9cd15808f80343eb4903b4da4
Zinou <zineddine.kermouz@redfabriq.com>
2021-11-24 09:45:59 +0100

    create info box for quiz category and difficulty

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//https://opentdb.com/api.php
+const category_select = document.getElementById("category");
+const difficulty_select = document.getElementById("difficulty");
+
+
+let selectedCategory = 'any';
+let selectedDifficulty = 'any';
+
+function categoryChanged() {
+    selectedCategory = category_select.value;
+}
+
+function difficultyChanged() {
+    selectedDifficulty = difficulty_select.value;
+}
+
+async function startQuiz() {
+    console.log('start quiz', selectedCategory, selectedDifficulty);
+}
